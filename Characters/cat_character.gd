0d8cdd65db6c4b79ee2f52a721974aa55c562691
james wallace <jawallace0392@gmail.com>
2023-03-09 10:46:14 -0500

    started better cow character

--- Characters/cat_character.gd
+++ Characters/cat_character.gd
@@ -6,21 +6,16 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-
 func _ready():
 	update_animation_parameters(starting_direction)
-	
-	
+
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	
 	velocity = input_direction.normalized() * move_speed
-	
 	update_animation_parameters(velocity)
-
 	pick_new_state(velocity)
 	move_and_slide()
 
@@ -28,7 +23,6 @@ func update_animation_parameters(move_input : Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-		
 
 func pick_new_state(velocity):
 	if(velocity != Vector2.ZERO):
